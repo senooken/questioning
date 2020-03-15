17e143eaccf014bf2ac5f49d99171662ed3b0691
SENOO, Ken <contact@senooken.jp>
2020-03-16 01:23:02 +0900

    Minimum user view only page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// /register, /login, /logout
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{user}', 'UserController@index');
