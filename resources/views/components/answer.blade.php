eb816e9ef858b7cf45139d15413813eb525893ea
SENOO, Ken <contact@senooken.jp>
2020-04-06 17:38:38 +0900

    Add profile link

--- resources/views/components/answer.blade.php
+++ resources/views/components/answer.blade.php
@@ -1,10 +1,12 @@
 <article>
     <figure>
         @isset ($card->a_username)
+        <a href="{{url('/user/'.$card->a_username)}}">
         <img src="{{\Storage::disk('public')->url(App\User::where('name', $card->a_username)->first()->avatar)}}"
              style="height: 3em;"
         />
         {{$card->a_username}}
+        </a>
         @endisset
         <blockquote><p>{{ $card->a_body }}</p></blockquote>
         <footer><small>{{ $card->a_updated_at }}</small></footer>
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -2,10 +2,12 @@
     <figure class="card-header">
         <header>
             @isset ($card->q_username)
+            <a href="{{url('/user/'.$card->q_username)}}">
             <img src="{{\Storage::disk('public')->url(App\User::where('name', $card->q_username)->first()->avatar)}}"
                  style="height: 3em;"
             />
             {{$card->q_username}}
+            </a>
             @endisset
         </header>
         <blockquote>
